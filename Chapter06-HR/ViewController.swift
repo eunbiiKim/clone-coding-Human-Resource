0fc8cecd326f7d7a6337b28696b5b2189c6bd5c3
eunbiiKim <eunbikim0807@gmail.com>
2020-12-16 19:49:07 +0900

    Initial Commit

---
+++ Chapter06-HR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter06-HR
+//
+//  Created by 김은비 on 2020/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
